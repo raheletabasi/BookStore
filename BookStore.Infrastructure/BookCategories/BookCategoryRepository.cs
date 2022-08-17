c7f6e1c15e4675fe5bbe80969ef6e6556bd830d3
Rahele <raheleh.tabasi@gmail.com>
2022-08-17 15:11:17 +0430

    [ADD] add bookCategory in infra layer add EntityTypeConfiguration for this model

---
+++ BookStore.Infrastructure/BookCategories/BookCategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using BookStore.Domain.BookCategories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Infrastructure.BookCategories;
+
+internal class BookCategoryRepository : IBookCategoryRepository
+{
+}
