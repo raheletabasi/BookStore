2c590e23790fc109f9e43b196bee0733e11baaa6
Rahele <raheleh.tabasi@gmail.com>
2022-08-18 00:53:50 +0430

    [ADD] add infra layer for role permission

---
+++ BookStore.Infrastructure/RolePermissions/RolePermissionEntityTypeConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using BookStore.Domain.RolePermissions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Infrastructure.RolePermissions
+{
+    public class RolePermissionEntityTypeConfiguration : IEntityTypeConfiguration<RolePermission>
+    {
+        public void Configure(EntityTypeBuilder<RolePermission> builder)
+        {
+            builder.ToTable("RolePermission");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.RoleId).IsRequired();
+            builder.Property(x => x.PermissionId).IsRequired();
+
+            #region Relations
+
+            builder.HasOne(x => x.Role);
+            builder.HasOne(x => x.Permission);
+
+            #endregion
+        }
+    }
+}
---
+++ BookStore.Infrastructure/RolePermissions/RolePermissionRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.Domain.RolePermissions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Infrastructure.RolePermissions
+{
+    internal class RolePermissionRepository : IRolePermissionRepository
+    {
+    }
+}
