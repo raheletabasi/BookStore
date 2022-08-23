7a3900db910e7532b3707001cd463cb5b91535ac
Rahele <raheleh.tabasi@gmail.com>
2022-08-23 23:16:56 +0430

    [ADD] add unitOfWork in infra layer and define all repository in it

---
+++ BookStore.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,48 @@
+﻿using BookStore.Domain.Interface;
+using BookStore.Domain.Repositories;
+using BookStore.Infrastructure.Data;
+
+namespace BookStore.Infrastructure;
+
+public class UnitOfWork : IUnitOfWork, IDisposable
+{
+    private readonly BookStoreContext _context;
+
+    public UnitOfWork(BookStoreContext context)
+    {
+        _context = context;
+    }
+
+    public IBookCategoryRepository BookCategories { get; }
+
+    public IBookRepository Books { get; }
+
+    public ICategoryRepository Categories { get; }
+
+    public IOrderDetailRepository OrderDetails { get; }
+
+    public IOrderRepository Orders { get; }
+
+    public IPermissionRepository Permissions { get; }
+
+    public IRolePermissionRepository RolePermission { get; }
+
+    public IRoleRepository Roles { get; }
+
+    public IUserRoleRepository UserRoles { get; }
+
+    public IUserRepository Users { get; }
+
+    public async Task<bool> Commit()
+    {
+        var success = (await _context.SaveChangesAsync()) > 0;
+        return success;
+    }
+
+    public void Dispose() => _context.Dispose();
+
+    public Task Rollback()
+    {
+        return Task.CompletedTask;
+    }
+}
