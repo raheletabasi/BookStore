f45a6b9523d209ff9bcbdde03e37eb8786cc601a
Rahele <raheleh.tabasi@gmail.com>
2022-08-23 22:14:51 +0430

    [ADD] add common folder in domain layer and define base entity and auditiable base entity

---
+++ BookStore.Domain/Common/AuditableBaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Domain.Common
+{
+    public abstract class AuditableBaseEntity : BaseEntity, IAuditableEntity
+    {
+        public bool IsDeleted { get; set; } = false;
+        public Guid CreatedUserId { get; set; }
+        public Guid ModifiedUserId { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime ModifiedDate { get; set; }
+    }
+}
