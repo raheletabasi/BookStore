892e5e809c8e39b5cf007f577f9483c83fd03e44
Rahele <raheleh.tabasi@gmail.com>
2022-08-23 23:21:35 +0430

    [ADD] add context class

---
+++ BookStore.Infrastructure/Context/BookStoreContext.cs
@@ -0,0 +1,25 @@
+﻿using BookStore.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Infrastructure.Data
+{
+    public class BookStoreContext : DbContext
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Author> Author { get; set; }
+        public DbSet<BookCategory> BookCategory { get; set; }
+        public DbSet<Book> Book { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<OrderDetail> OrderDetail { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<Permission> Permission { get; set; }
+        public DbSet<RolePermission> RolePermission { get; set; }
+        public DbSet<Role> Role { get; set; }
+        public DbSet<UserRole> UserRole { get; set; }
+        public DbSet<User> User { get; set; }
+    }
+}
