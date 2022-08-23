c1d91ada75f88344e43ab6ba7b6ee069e13d9dd7
Rahele <raheleh.tabasi@gmail.com>
2022-08-23 23:11:21 +0430

    [ADD] add Repository for all entities and create generic repository

---
+++ BookStore.Infrastructure/Repositories/BookRepository.cs
@@ -0,0 +1,23 @@
+﻿using BookStore.Domain.Entity;
+using BookStore.Domain.Repositories;
+using BookStore.Infrastructure.Data;
+using BookStore.Infrastructure.GenericRepository;
+
+namespace BookStore.Infrastructure.Repositories;
+
+internal class BookRepository : GenericRepository<Book>, IBookRepository
+{
+    public BookRepository(BookStoreContext context) : base(context)
+    {
+    }
+
+    public Task IsDuplicate(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task IsDuplicate(Guid Id, string name)
+    {
+        throw new NotImplementedException();
+    }
+}
