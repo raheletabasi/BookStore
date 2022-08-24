f99fcc5109f7db92067f4c5a6433cee03915eea3
Rahele <raheleh.tabasi@gmail.com>
2022-08-24 15:27:43 +0430

    [ADD] add DTO for login user

---
+++ BookStore.Application/DTOs/LoginUserViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Application.DTOs;
+
+public class LoginUserViewModel
+{
+    [Display(Name = "نام کاربری")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+    [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+    public string UserName { get; set; }
+
+    [Display(Name = "ایمیل")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+    [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+    public string Email { get; set; }
+
+    [Display(Name = "رمزعبور")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+    [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+    public string Password { get; set; }
+
+    [Display(Name = "مرا به خاطر بسپار")]
+    public bool RememberMe { get; set; }
+}
+
+public enum ResultLoginUser
+{ 
+    success,
+    notFound,
+    inactive,
+    block,
+    Error
+}
