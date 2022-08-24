473822cdbf21975c69fd88e5862153d4ad18d253
Rahele <raheleh.tabasi@gmail.com>
2022-08-24 15:22:47 +0430

    [ADD] add user service and implement get user by id and all user and register user and login user and edit profile and change password

---
+++ BookStore.Application/Services/UserService.cs
@@ -0,0 +1,130 @@
+﻿using BookStore.Application.DTOs;
+using BookStore.Application.Interfaces;
+using BookStore.Application.Security;
+using BookStore.Domain.Entity;
+using BookStore.Domain.Interface;
+
+namespace BookStore.Application.Services;
+
+public class UserService : IUserService
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UserService(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<ResultChangePassword> ChangePassword(Guid userId, ChangePasswordViewModel changePasswordViewModel)
+    {
+        try
+        {
+            var user = await _unitOfWork.Users.GetByIdAsync(userId);
+            if (user != null)
+            {
+                var newPass = PasswordHelper.EncodePasswordMd5(changePasswordViewModel.NewPassword);
+                if (user.Password != newPass)
+                {
+                    user.Password = newPass;
+                    await _unitOfWork.Users.UpdaeAsync(user);
+                    await _unitOfWork.Commit();
+
+                    return ResultChangePassword.success;
+                }
+                return ResultChangePassword.equalPassword;
+            }
+            return ResultChangePassword.notFound;
+        }
+        catch
+        {
+            return ResultChangePassword.error;
+        }
+    }
+
+    public async Task<ResultEditUserProfile> EditProfile(Guid userId,EditUserProfileViewModel editUserProfileViewModel)
+    {
+        try
+        {
+            var user = await _unitOfWork.Users.GetByIdAsync(userId);
+            if (user == null) return ResultEditUserProfile.notFound;
+
+            user.FirstName = editUserProfileViewModel.FirstName;
+            user.LastName = editUserProfileViewModel.LastName;
+            user.Mobile = editUserProfileViewModel.Mobile;
+            user.Gender = editUserProfileViewModel.Gender;
+            user.Birthdate = editUserProfileViewModel.Birthdate;
+            user.State = editUserProfileViewModel.State;
+            user.City = editUserProfileViewModel.City;
+            user.Address = editUserProfileViewModel.Address;
+            user.PostalCode = editUserProfileViewModel.PostalCode;
+
+            await _unitOfWork.Users.UpdaeAsync(user);
+            await _unitOfWork.Commit();
+
+            return ResultEditUserProfile.success;
+        }
+        catch
+        {
+            return ResultEditUserProfile.error;
+        }
+    }
+
+    public async Task<IEnumerable<User>> GetAllUser()
+    {
+        return await _unitOfWork.Users.GetAllAsync();
+    }
+
+    public async Task<User> GetUserByUserId(Guid userId)
+    {
+        return await _unitOfWork.Users.GetByIdAsync(userId);
+    }
+
+    public async Task<ResultLoginUser> LoginUser(LoginUserViewModel loginUserViewModel)
+    {
+        try
+        {
+            var user = await _unitOfWork.Users.GetUserByEmail(loginUserViewModel.Email);
+            if (user == null) return ResultLoginUser.notFound;
+            if (user.Status == UserStatus.Inactive) return ResultLoginUser.inactive;
+            if (user.Status == UserStatus.Block) return ResultLoginUser.block;
+
+            return ResultLoginUser.success;
+        }
+        catch
+        {
+            return ResultLoginUser.Error;
+        }
+    }
+
+    public async Task<ResultRegisterUser> RegisterUser(RegisterUserViewModel registerViewModel)
+    {
+        try
+        {
+            var isDuplicateUser = await _unitOfWork.Users.IsDuplicate(registerViewModel.UserName, registerViewModel.Email);
+            if (!isDuplicateUser)
+            {
+                var newUser = new User
+                {
+                    FirstName = registerViewModel.FirstName,
+                    LastName = registerViewModel.LastName,
+                    Email = registerViewModel.Email,
+                    UserName = registerViewModel.UserName,
+                    Mobile = registerViewModel.Mobile,
+                    Password = PasswordHelper.EncodePasswordMd5(registerViewModel.Password)
+                };
+
+                await _unitOfWork.Users.AddAsync(newUser);
+                await _unitOfWork.Commit();
+
+                return ResultRegisterUser.success;
+            }
+            return ResultRegisterUser.Duplicate;
+        }
+        catch
+        {
+            return ResultRegisterUser.Error;
+        }
+
+
+    }
+}
