3b2bb32ff82f78d8f3ab5ea4eb1313d67ac03389
Rahele <raheleh.tabasi@gmail.com>
2022-08-27 10:46:32 +0430

    [ADD] add order detail repository and service and implement service and at the end create controller for order detail like get post put delete

---
+++ BookStore.Application/DTOs/OrderDetailViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Application.DTOs;
+
+public class OrderDetailViewModel
+{
+    public Guid Id { get; set; }
+
+    [Display(Name = "سفارش")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+    public Guid OrderId { get; set; }
+
+    [Display(Name = "کتاب")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+    public Guid BookId { get; set; }
+
+    [Display(Name = "تعداد")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+    public decimal Qty { get; set; }
+
+    [Display(Name = "مبلغ")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+    public decimal Price { get; set; }
+}
+
+public enum ResultOrderDetail
+{
+    success,
+    duplicate,
+    notFound,
+    Error
+}
---
+++ BookStore.Application/Interfaces/IOrderDetailService.cs
@@ -0,0 +1,12 @@
+﻿using BookStore.Application.DTOs;
+using BookStore.Domain.Entity;
+
+namespace BookStore.Application.Interfaces;
+
+public interface IOrderDetailService
+{
+    Task<ResultOrderDetail> AddOrderDetail(OrderDetailViewModel orderDetailViewModel);
+    Task<ResultOrderDetail> DeleteOrderDetail(Guid id);
+    Task<ResultOrderDetail> UpdateOrderDetail(OrderDetailViewModel orderDetailViewModel);
+    Task<IEnumerable<OrderDetail>> GetOrderDetailByOrderId(Guid orderId);
+}
