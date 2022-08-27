9549f66513218854febdb54a9845ec03602e9b50
Rahele <raheleh.tabasi@gmail.com>
2022-08-27 11:11:17 +0430

    [ADD] add folder utility in application layer for datetime and string helper

---
+++ BookStore.Application/Utiliy/StringHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.Utiliy;
+
+public static class StringHelper
+{
+    public static string FixText(this string text) => text?.Trim().Replace(" ", "");
+}
