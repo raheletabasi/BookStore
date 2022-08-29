ee19bbf020dc0116168a0084c56e33c67d10502b
Rahele <raheleh.tabasi@gmail.com>
2022-08-29 09:35:56 +0430

    [ADD],[FIX] Add is exist method and change name update method

--- BookStore.Domain/Interface/Repositories/GenericRepository/IGenericRepository.cs
+++ BookStore.Domain/Interface/Repositories/GenericRepository/IGenericRepository.cs
@@ -5,10 +5,11 @@ namespace BookStore.Domain.Repositories.Generic;
 public interface IGenericRepository<TEntity> where TEntity : BaseEntity
 {
     public Task AddAsync(TEntity entity);
-    public Task UpdaeAsync(TEntity entity);
+    public Task UpdateAsync(TEntity entity);
     public Task DeleteAsync(TEntity entity);
     public Task<IEnumerable<TEntity>> GetAllAsync();
     public Task<TEntity> GetByIdAsync(Guid id);
     public Task SoftDelete(Guid id);
+    public Task<bool> IsExist(Guid id);
     public Task Save();
 }
